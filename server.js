9fe559b0cd20549dd6337f4933c2a5835f689778
aadi-254 <161030423+aadi-254@users.noreply.github.com>
2025-03-09 02:49:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 const path = require("path");
-const PORT = process.env.PORT || 3000
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;  
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
